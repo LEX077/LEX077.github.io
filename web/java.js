e0a36247c4b3b333a54df8f670a1f25791f3842a
lex077 <74213068@certus.edu.pe>
2023-06-26 04:20:19 -0500

    "java"

---
+++ web/java.js
@@ -0,0 +1,9 @@
+function abrirFormulario() {
+    var modal = document.getElementById('modal');
+    modal.style.display = 'block'; // Mostrar el contenedor modal
+}
+
+function cerrarFormulario() {
+    var modal = document.getElementById('modal');
+    modal.style.display = 'none'; // Ocultar el contenedor modal
+}
